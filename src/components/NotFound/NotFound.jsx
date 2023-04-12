f2e5f0917e49c17d6b67574116d8863986aaaad1
Rahul Saha <rahulsaha87928@gmail.com>
2023-04-12 20:50:10 +0600

    final commit with some modify

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const NotFound = () => {
     return (
         <div className='text-center mt-10 flex justify-center'>
-            <img className='' src="/public/n.jpg" alt="" />
+            <img src="n.jpg" alt="" />
         </div>
     );
 };
