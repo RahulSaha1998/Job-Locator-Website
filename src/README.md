76dac7516333edf5051ca313c87922355219027c
Rahul Saha <rahulsaha87928@gmail.com>
2023-04-12 20:03:25 +0600

    filtered data working well

--- src/README.md
+++ src/README.md
@@ -1,8 +1,9 @@
-## Hello there is some features and live website link
+## Hello there is some features and live website link of this project.
 
 - First we create a navbar and there contains a name and some links and the navbar is common for all the roots.
 - Than the banner section there is a graduate picture of mine and some titles.
 - Then the job category section. it will contain 4 json data and i fetch the data to show the UI.
 - Then Features Job it will contain 6 json data and first fetch 4 data to the UI and if i click See all jobs it will show full data.
 - it contains a button show details if i click it will show the details of the data.
-- and then I can apply the jobs and and it will show in applied jobs.
+- and then I can apply the jobs and and it will show in applied jobs and saved the data in local storage.
+- And then fetch the local storage data to applied jobs route. 
--- src/components/Applied-jobs/AppliedJobs.jsx
+++ src/components/Applied-jobs/AppliedJobs.jsx
@@ -4,44 +4,37 @@ import DisplayAppliedJobs from '../DisplayAppliedJobs/DisplayAppliedJobs';
 
 const AppliedJobs = () => {
     const appliedData = useLoaderData();
-    // console.log(appliedData);
-
-    const [applied, setAppliedData] = useState(appliedData);
-    const [data, setData] = useState([])
-    const [data1, setData1] = useState([]);
-
-    const handleButton = () =>{
-        setAppliedData(appliedData);
-        console.log(applied);
+    const [filteredData, setFilteredData] = useState(appliedData);
+    const handleFilter = (filter) => {
+            const filtered = appliedData.filter(job => job.remote_or_onsite === filter);
+            setFilteredData(filtered);
+        
     }
 
-    const clickButton = () =>{
-        setData('remote')
-        console.log(data);
+    const handleFilter2 = () =>{
+        setFilteredData(appliedData)
     }
-
-    useEffect(()=>{
-        const remote = appliedData.filter(dt => dt.remote_or_onsite == data);
-        setData1(remote);
-        console.log(data1);
-    },[appliedData])
 
     return (
         <div>
             <div>
                 <div className='text-center mt-12'>
                     <h2 className='text-3xl font-bold'>Applied Jobs</h2>
-                    <div className='md:text-center lg:text-end lg:mr-96 m-5'>
-                        <button className='btn btn-info mx-2' onClick={clickButton}>Remote</button>
-                        <button className='btn btn-info'>Onsite</button>
+                    <div className='text-center  m-5'>
+                        {/* Buttons for filtering */}
+                        <button onClick={() => handleFilter('Remote')} className='mr-2 btn btn-info'>Remote</button>
+                        <button className='btn btn-info' onClick={() => handleFilter('Onsite')}>OnSite</button>
+                        <button className='ml-2 btn btn-info' onClick={handleFilter2}>All Data</button>
                     </div>
                 </div>
                 <div className='mt-5 lg:mx-20'>
                     {
-                        appliedData.map(job => <DisplayAppliedJobs
-                            key={job.id}
-                            job={job}
-                        ></DisplayAppliedJobs>)
+                        filteredData.map(job => (
+                            <DisplayAppliedJobs
+                                key={job.id}
+                                job={job}
+                            ></DisplayAppliedJobs>
+                        ))
                     }
                 </div>
             </div>
@@ -51,58 +44,6 @@ const AppliedJobs = () => {
 
 export default AppliedJobs;
 
-
-
-
-
-
-///////////3rd Step
-
-// import React, { useState } from 'react';
-// import { useLoaderData } from 'react-router-dom';
-// import DisplayAppliedJobs from '../DisplayAppliedJobs/DisplayAppliedJobs';
-
-// const AppliedJobs = () => {
-//     const appliedData = useLoaderData();
-//     const [filteredData, setFilteredData] = useState(appliedData); // State to track filtered data
-
-//     // Function to handle button click
-//     const handleButtonClick = (option) => {
-//         const filteredJobs = appliedData.filter(job => job.remote_or_onsite.target.value === option);
-//         setFilteredData(filteredJobs);
-//     }
-
-//     return (
-//         <div>
-//             <div>
-//                 <div className='text-center mt-12'>
-//                     <h2 className='text-3xl font-bold'>Applied Jobs</h2>
-//                 </div>
-//                 <div className='mt-5 mx-20'>
-//                     <div className='flex justify-center space-x-5'>
-//                         <button
-//                             className='border px-4 py-2 rounded-full bg-blue-500 text-white'
-//                             onClick={() => handleButtonClick('remote')}
-//                         >
-//                             Remote
-//                         </button>
-//                         <button
-//                             className='border px-4 py-2 rounded-full bg-blue-500 text-white'
-//                             onClick={() => handleButtonClick('onsite')}
-//                         >
-//                             Onsite
-//                         </button>
-//                     </div>
-//                     {filteredData.map(job => (
-//                         <DisplayAppliedJobs key={job.id} job={job} />
-//                     ))}
-//                 </div>
-//             </div>
-//         </div>
-//     );
-// };
-
-// export default AppliedJobs;
 
 
 
